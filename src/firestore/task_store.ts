c8d87e96ab9533f63106116c760c2177e71dd470
Issei Kuzumaki <mail@isseikuzumaki.tokyo>
2022-08-11 23:53:14 +0900

    tmp

--- src/firestore/task_store.ts
+++ src/firestore/task_store.ts
@@ -2,42 +2,19 @@ import {
     DocumentData,
     FirestoreDataConverter,
     QueryDocumentSnapshot,
-    SnapshotOptions,
     doc,
     getFirestore,
     setDoc,
     collection,
     getDocs,
     getDoc
-} from 'firebase/firestore'
+} from '@firebase/firestore/lite'
 import { Task } from '../data'
 import { sendDebug, sendError } from '../github'
 
-const taskConverter: FirestoreDataConverter<Task> = {
-    toFirestore(data: Task): DocumentData {
-        return {
-            branch: data.branch,
-            commit: data.commit,
-            issue: data.issue,
-            user: data.user,
-        }
-    },
-    fromFirestore(snapshot: QueryDocumentSnapshot, options: SnapshotOptions): Task {
-        const data = snapshot.data(options)
-        // Book オブジェクトの id プロパティには Firestore ドキュメントの id を入れる。
-        return {
-            id: snapshot.id,
-            branch: data.branch,
-            commit: data.commit,
-            issue: data.issue,
-            user: data.user,
-        }
-    },
-}
-
 export async function addTask(data: Task) {
     const db = getFirestore()
-    const docRef = doc(db, 'tasks', data.id).withConverter(taskConverter)
+    const docRef = doc(db, 'tasks', data.id)
     await setDoc(docRef, data)
 }
 
